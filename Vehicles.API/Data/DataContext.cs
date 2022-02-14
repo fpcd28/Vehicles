a11a1613b69042ede6b732feaf96526cea602171
fpcd <fpcd28@gmail.com>
2022-02-14 17:32:34 -0500

    Create crud de procedure

--- Vehicles.API/Data/DataContext.cs
+++ Vehicles.API/Data/DataContext.cs
@@ -11,16 +11,19 @@ namespace Vehicles.API.Data
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
-
         }
 
+        public DbSet<Procedure> Procedures { get; set; }
+
         public DbSet<VehicleType> VehicleTypes { get; set; }
+
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Procedure>().HasIndex(x => x.Description).IsUnique();
             modelBuilder.Entity<VehicleType>().HasIndex(x => x.Description).IsUnique();
-
         }
 
     }
---
+++ Vehicles.API/Data/Entities/Procedure.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vehicles.API.Data.Entities
+{
+    public class Procedure
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Procedimiento")]
+        [MaxLength(50, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        [Required(ErrorMessage = "El Campo {0} es obligatorio.")]
+        public String Description { get; set; }
+
+        [Display(Name = "Precio")]
+        [DisplayFormat(DataFormatString ="{0:C2}")]
+        [Required(ErrorMessage = "El Campo {0} es obligatorio.")]
+        public decimal Price { get; set; }
+
+    }
+}
